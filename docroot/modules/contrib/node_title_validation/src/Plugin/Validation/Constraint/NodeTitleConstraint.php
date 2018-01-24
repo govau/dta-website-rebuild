affd2ceee4e7c7550ceefb76c325eb6dcbf5db51
Jesse Boyd <jesse.boyd@digital.gov.au>
2018-01-24 16:14:23 +1100

    Added node_title_help_text and node_title_validation modules.

---
+++ docroot/modules/contrib/node_title_validation/src/Plugin/Validation/Constraint/NodeTitleConstraint.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\node_title_validation\Plugin\Validation\Constraint\ForumLeafConstraint.
+ */
+
+namespace Drupal\node_title_validation\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Checks that the node.
+ *
+ * @Constraint(
+ *   id = "NodeTitleValidate",
+ *   label = @Translation("List of items"),
+ * )
+ */
+class NodeTitleConstraint extends Constraint {
+
+}
