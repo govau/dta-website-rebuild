e5d30858824107654e00dff7c01eea88e9833a03
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-02-23 12:46:03 +1100

    Add set -v

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,11 @@
 # Exit immediately if there is an error
 set -e
 
-# cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
+# Cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
 set -o pipefail
+
+# Print shell input lines as they are read.
+set -v
 
 # Include build env vars
 source "$(dirname "$0")/buildrc"
