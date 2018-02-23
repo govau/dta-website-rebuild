4487e9a7c29234922c19f6364fcfddc26999a40e
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-02-23 12:24:44 +1100

    Remove debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 # Exit immediately if there is an error
-set -ex
+set -e
 
 # cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
 set -o pipefail
 
 # Include build env vars
 source "$(dirname "$0")/buildrc"
-
-env
 
 # login to cloud foundry if env vars are present
 login() {
