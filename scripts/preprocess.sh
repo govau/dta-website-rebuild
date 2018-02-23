40aaf8fd58aa6b8e53b0cdb2ac5911ceef62d06b
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-02-23 15:04:47 +1100

    Add setting of uuid

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -9,12 +9,21 @@ set -o pipefail
 # echo out each line of the shell as it executes
 set -x
 
-# This script is executed from .bp-config/options.json via ADDITIONAL_PREPROCESS_CMDS
+# This script is executed each running container from .bp-config/options.json via ADDITIONAL_PREPROCESS_CMDS
 # See https://docs.cloudfoundry.org/buildpacks/php/gsg-php-config.html#options
 
 # Only execute on the first application instance
 if [[ "${CF_INSTANCE_INDEX}" = "0" ]]; then
   echo "I am the first instance"
+
+  # If DRUPAL_UUID is defined, change our UUID to it if necessary
+  if [[ -n ${DRUPAL_UUID+x} ]]; then
+    CURRENT_UUID=$(./bin/drush config-get "system.site" uuid)
+    if [[ "${DRUPAL_UUID}" != "${CURRENT_UUID}" ]]; then
+      ./bin/drush config-set "system.site" uuid "${DRUPAL_UUID}"
+    fi
+  fi
+
 else
   echo "I am not the first instance"
 fi
