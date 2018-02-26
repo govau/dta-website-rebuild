b02d6c3ab0b247b1ff2f99904950bd02c30e5bfd
Brendan <2324569+gordcorp@users.noreply.github.com>
2018-02-26 16:23:18 +1100

    Tweaks to circle and add support for running drush in cloud foundry (#87)
    * Add env vars to match composer
    
    * Use composer within cf staging
    
    * Simplify circle workflow
    
    * Add preprocess script
    
    * Make drush available in non-dev environments
    
    * Print out the env to help with debug
    
    * sigh ckeditor s/http/https/g
    
    * Only deploy circle branch for now
    
    * Fix script path
    
    * Simplify deploy scripts and use zero-downtime
    
    * Add env for debugging
    
    * Fix GIT_REPO when run on circle
    
    * Remove debug
    
    * Dont ignore scripts
    
    * Add set -v
    
    * Add cache-rebuild
    
    * Remove env
    
    * Fix instance check
    
    * Add setting of uuid
    
    * Add mysql cli
    
    * Downgrade to drush 8
    
    * sigh ckeditor s/http/https/g
    
    * Increase timeout
    
    * Add mysql and fix config-import
    
    * cd to docroot for the whole script
    
    * Increase instances to 2
    
    * Change back to drush 9
    
    * Use sed to hotfix http->https
    
    * Remove commented lines

---
+++ scripts/deploy.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Exit immediately if there is an error
+set -e
+
+# Cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
+set -o pipefail
+
+# Print shell input lines as they are read.
+set -v
+
+# Include build env vars
+source "$(dirname "$0")/buildrc"
+
+# login to cloud foundry if env vars are present
+login() {
+
+  if [[ -z "$CF_USER" ]]; then
+    echo "CF env vars not found, assuming you are already logged in to cf"
+    return
+  fi
+
+  if [[ "${GIT_BRANCH}" = "master" ]]; then
+    cf api $CF_PROD_API
+  else
+    cf api $CF_STAGING_API
+  fi
+
+  cf auth $CF_USER $CF_PASSWORD
+  cf target -o $CF_ORG
+  cf target -s $CF_SPACE
+}
+
+# main script function
+#
+main() {
+
+  login
+
+  case "${GIT_BRANCH}" in
+    master)
+      cf zero-downtime-push dta-website-rebuild -f manifest-master.yml
+      ;;
+    develop)
+      cf zero-downtime-push dta-website-rebuild -f manifest-develop.yml
+      ;;
+    *)
+      cf zero-downtime-push ${GIT_REPO}-`basename "${GIT_BRANCH}"` -f manifest.yml
+      ;;
+  esac
+
+}
+
+main $@
