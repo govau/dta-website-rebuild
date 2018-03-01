4d5ec397130af2e92466aa12679b7970179a55f1
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-03-01 14:14:23 +1100

    Only run php lint on modified php files

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,15 +15,25 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Add vendored bin dir to PATH
 PATH="${SCRIPT_DIR}/../bin:${PATH}"
 
-pushd docroot
+if [ -n "$CIRCLE_BRANCH" ]; then
+  # For efficiency we only want to run certain tests against deviations from
+  # the base branch, which for now we assume is develop.
+  BASE_BRANCH=develop
+  if [[ $CIRCLE_BRANCH != $BASE_BRANCH ]] ; then
 
-  find . -name \*.php ! -name global_namespace_php5.php -type f -print0 | xargs -0 -n1 php -l
+    #Run php lint, but only against modified php files
+    git diff --name-only origin/${BASE_BRANCH}... -- '*.php' | xargs -n1 php -l
 
-  pushd core
+    # If anything in core has changed, run its phpunit
+    for file in $(git diff --name-only origin/${BASE_BRANCH}...); do
+      if [[ $file == docroot/core* ]]; then
+        pushd docroot/core
+          phpunit --testsuite=unit --exclude-group \
+            Composer,DependencyInjection,PageManager,jsonapi
+        popd
+        break
+      fi
+    done
 
-    phpunit --testsuite=unit --exclude-group \
-      Composer,DependencyInjection,PageManager,jsonapi
-
-  popd
-
-popd
+  fi
+fi
