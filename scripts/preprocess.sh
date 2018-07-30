11520c0e1b44470a5d623725b2b3a5b92d7ab516
Jesse Boyd <jesse.boyd@digital.gov.au>
2018-07-30 15:11:26 +1000

    Updated scripts This commit adds a new `scripts/start.sh` which is launched from `manifest.yml`. This moves all of the functionality which was previously in `preprocess.sh` out into a separate script, which is only launched when the app is started, rather than on every action (ie `run-task`).

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -13,102 +13,14 @@ set -o pipefail
 # echo out each line of the shell as it executes
 set -x
 
+echo "preprocess.sh started"
+
 # Add included mysql cli to PATH
 PATH="${HOME}/scripts/mysql:${PATH}"
 
 # Add vendored bin dir to PATH
 PATH="${HOME}/bin:${PATH}"
 
-cd docroot
-
-# Only execute on the first application instance
-if [[ "${CF_INSTANCE_INDEX}" = "0" ]]; then
-  echo "I am the first instance"
-  CACHE_FLAG="false"
-  # If DRUPAL_UUID is defined, change our UUID to it if necessary
-  if [[ -n ${DRUPAL_UUID+x} ]]; then
-    CURRENT_UUID=$(drush cget "system.site" --format=json | jq -r .uuid )
-    if [[ ${DRUPAL_UUID} != ${CURRENT_UUID} ]]; then
-      drush config-set -y "system.site" uuid "${DRUPAL_UUID}"
-    fi
-  fi
-
-  # Import the config from sync dir
-  config_status_file=$(mktemp)
-
-  CONFIG_STATUS=$(drush config-status &>$config_status_file)
-
-  printf "%s\n" "$CONFIG_STATUS"
-
-  haystack=$(< $config_status_file)
-
-  case "${haystack}" in
-    *"No differences"*)
-      printf "%s\n" "$CONFIG_STATUS"
-      exit
-      ;;
-    *)
-      echo "Updates required."
-      printf "%s\n" "$CONFIG_STATUS"
-      drush config-import -y
-      CACHE_FLAG="true"
-      ;;
-  esac
-
-  # Run updatedb if necessary
-  UPDATEDB_STATUS=$(drush updatedb-status 2>/dev/null)
-  if [[ $UPDATEDB_STATUS != "" ]]; then
-    printf "%s\n" "$UPDATEDB_STATUS"
-    echo "Updates required"
-    error_file=$(mktemp)
-    UPDATEDB_OUTPUT+=$(drush updatedb -y --entity-updates --no-cache-clear 2>$error_file)
-    printf "%s\n" "$UPDATEDB_OUTPUT"
-    err=$(< $error_file)
-    case "${err}" in
-      *"Update failed"*)
-        echo "An error occured!"
-        printf "%s\n" "$UPDATEDB_OUTPUT"
-        exit 1
-        ;;
-      *)
-        echo "Updates performed without error. Please check output."
-        printf "%s\n" "$UPDATEDB_OUTPUT"
-        CACHE_FLAG="true"
-        ;;
-    esac
-  else
-    echo "No updates required."
-  fi
-
-  # Uninstall modules on certain environments.
-  if [[ -n ${ENVIRONMENT+x} ]]; then
-    echo "Currently running in the ${ENVIRONMENT} environment."
-    if [[ ${ENVIRONMENT} = "staging" ]] || [[ ${ENVIRONMENT} = "production" ]]; then
-      DEVEL_STATUS=$(drush pm-list --pipe --type=module --status=enabled --no-core --fields=name | grep "devel")
-      if [[ $DEVEL_STATUS != "" ]]; then
-        drush pm-uninstall devel -y
-        CACHE_FLAG="true"
-      fi
-      KINT_STATUS=$(drush pm-list --pipe --type=module --status=enabled --no-core --fields=name | grep "kint")
-      if [[ $KINT_STATUS != "" ]]; then
-        drush pm-uninstall kint -y
-        CACHE_FLAG="true"
-      fi
-      LINK_CSS_STATUS=$(drush pm-list --pipe --type=module --status=enabled --no-core --fields=name | grep "link_css")
-      if [[ $LINK_CSS_STATUS != "" ]]; then
-        drush pm-uninstall link_css -y
-        CACHE_FLAG="true"
-      fi
-    fi
-  else
-    echo "Currently running in a local environment (or an environment without the correct environment variables set!)"
-  fi
-  # Clear the caches if required.
-  if [ "$CACHE_FLAG" = "true" ]; then
-    drush cache-rebuild
-  fi
-else
-  echo "I am not the first instance"
-fi
+echo "Path updated to ${PATH}"
 
 echo "preprocess.sh finished"
