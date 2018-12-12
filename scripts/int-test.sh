7b882dca4f8d1cd79cefd50e71ceae321c5eaf05
Jesse Boyd <jesse.boyd@digital.gov.au>
2018-12-12 11:24:17 +1100

    Include all tests

--- scripts/int-test.sh
+++ scripts/int-test.sh
@@ -13,5 +13,4 @@ set -x
 # Include build env vars
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/buildrc"
 
-behat --strict --stop-on-failure --config tests/behat/behat.yml --suite=ui
-behat --strict --stop-on-failure --config tests/behat/behat.yml --suite=user
+behat --strict --stop-on-failure --config tests/behat/behat.yml
