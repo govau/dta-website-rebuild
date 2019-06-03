ba0339b6128442aa98a6ff5206d6905ea63bee84
Jesse Boyd <jesse.boyd@digital.gov.au>
2019-06-03 10:49:00 +1000

    Adds a script to run the post-deploy commands once the deployment is complete.

---
+++ scripts/post-deploy.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Exit immediately if there is an error
+set -e
+
+# Cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
+set -o pipefail
+
+# Print shell input lines as they are read.
+set -v
+
+# Include build env vars
+source "$(dirname "$0")/buildrc"
+
+# login to cloud foundry if env vars are present
+login() {
+
+  if [[ -z "$CF_ORG" ]]; then
+    echo "CF env vars not found, assuming you are already logged in to cf"
+    return
+  fi
+
+  if [[ "${GIT_BRANCH}" = "master" ]]; then
+    cf api $CF_PROD_API
+    cf auth "$CF_USER" "$CF_PASSWORD_PROD"
+  else
+    cf api $CF_STAGING_API
+    cf auth "$CF_USER" "$CF_PASSWORD_STAGING"
+  fi
+
+  cf target -o $CF_ORG
+  cf target -s $CF_SPACE
+}
+
+# main script function
+#
+main() {
+
+  login
+
+  case "${GIT_BRANCH}" in
+    master)
+      CF_APP=dta-website
+      ;;
+    develop)
+      CF_APP=dta-website-staging
+      ;;
+    *)
+      CF_APP=${GIT_REPO}-`basename "${GIT_BRANCH}"`
+      ;;
+  esac
+
+  cf run-task ${CF_APP} "source scripts/buildrc && drush updatedb -y && drush cim -y && drush cr"
+
+}
+
+main $@
